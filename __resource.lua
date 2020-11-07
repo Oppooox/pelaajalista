ba68d2250f419e6349a5e6504b8d5ca616a16415
^OdebOi 3 <70506096+OdebOi@users.noreply.github.com>
2020-11-07 15:05:18 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Pelaajalista'
+
+version '1.0.0'
+
+server_script 'server/main.lua'
+
+client_script 'client/main.lua'
+
+ui_page 'html/pelaajalista.html'
+
+files {
+	'html/pelaajalista.html',
+	'html/muokkaa.css',
+	'html/listener.js',
+    'html/bg,jpg'
+}
